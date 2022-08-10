08751f9e4d45cc35eddcdc6676203e3884c71cff
SirObby <70753342+SirObby@users.noreply.github.com>
2022-08-10 17:18:00 +0800

    Added Engine, Studio and .gitmodules

---
+++ Engine/src/main.c
@@ -0,0 +1,10 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*int main(int argc, char **argv) {
+	printf("Loading\n");
+}*/
